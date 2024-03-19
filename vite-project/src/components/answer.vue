da617af30bdd579aac9d0224f6f14024a3a1024f
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2024-03-19 13:12:26 -0400

    commit

---
+++ vite-project/src/components/answer.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+const qAnswer = ref('')
+const input = ref(null)
+function receiveInput(){
+  qAnswer.value = input.value.value;
+  console.log(qAnswer.value)
+}
+</script>
+
+<template>
+  <form class="input">
+    <label>please enter answer:</label>
+    <input type="promptAnswer" ref="input">
+    <button  @click.prevent="receiveInput()">submit</button>
+  </form>
+  <div v-if="qAnswer != ''">hehehehaw</div>
+</template>
+
+<style scoped>
+
+</style>
