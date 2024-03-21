d04c8e494a1dcb55a38be469bc9df7f8f0a3477c
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2024-03-21 13:13:48 -0400

    commit

--- vite-project/src/components/answer.vue
+++ vite-project/src/components/answer.vue
@@ -5,7 +5,6 @@ const qAnswer = ref('')
 const input = ref(null)
 function receiveInput(){
   qAnswer.value = input.value.value;
-  console.log(qAnswer.value)
 }
 
 const player1Score = ref(0)
@@ -15,17 +14,33 @@ const player1votes = ref(0)
 const player2votes = ref(0)
 
 function addVote(buttonID){
-    if(buttonID === player1Answer){
-        player1votes += 1;
-        player2AnswerV:disabled = isDisabled;
-        console.log("player 1 has ", player1votes, "votes!");
+  console.log(buttonID)
+    if(buttonID === 1){
+        player1votes.value += 1;
+        player2AnswerV.disabled = true;
+        player1AnswerV.disabled = true;
+        console.log("player 1 has ", player1votes.value, "vote(s)!");
     }
-    else if(buttonID === player2Answer){
-        player2votes += 1;
-        player1AnswerV:disabled = isDisabled;
-        console.log("player 2 has ", player1votes, "votes!")
+    else if(buttonID === 2){
+        player2votes.value += 1;
+        player1AnswerV.disabled = true;
+        player2AnswerV.disabled = true;
+        console.log("player 2 has ", player2votes.value, "vote(s)!")
+    }
+    else{
+      console.log("it didnt work")
     }
 }
+
+function addScore(){
+  if(player1votes.value > player2votes.value){
+    player1Score.value = player1votes.value * 100 
+  }
+  else if(player2votes.value > player1votes.value){
+    player2Score.value = player2votes.value * 100 
+  }
+}
+
 </script>
 
 <template>
@@ -33,14 +48,14 @@ function addVote(buttonID){
   <form class="input">
     <label>please enter answer:</label>
     <input type="promptAnswer" ref="input">
-    <button  @click.prevent="receiveInput()">submit</button>
+    <button @click.prevent="receiveInput()">submit</button>
   </form>
   <br>
   <div>this is the prompt. what did you answer?</div>
   <div id="player1Answer" v-if="qAnswer != ''">{{ qAnswer }}</div>
-  <button id="player1AnswerV" v-if="qAnswer != ''" :disabled="notDisabled">Vote Player 1</button>
+  <button id="player1AnswerV" v-if="qAnswer != ''" @click="addVote(1)">Vote Player 1</button>
   <div id="player2Answer" v-if="qAnswer != ''">the answer of player 2</div>
-  <button id="player2AnswerV" v-if="qAnswer != ''" :disabled="notDisabled">Vote Player 2</button>
+  <button id="player2AnswerV" v-if="qAnswer != ''" @click="addVote(2)">Vote Player 2</button>
 </template>
 
 <style scoped>
