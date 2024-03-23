e4119fc280f275ad1f8e78441fe7820d9111b26a
noaha90 <noahabb@outlook.com>
2024-03-23 16:09:05 -0400

    ewq
    aew

--- vite-project/src/views/g1.vue
+++ vite-project/src/views/g1.vue
@@ -1,25 +1,28 @@
 <template>
     <div>
 <p>awesome</p>
+<button v-if="route.params.auth == 'host'"></button>
     </div>
 </template>
 
 <script setup>
-import { getDatabase, ref as r, set, onDisconnect,onValue, update, get, child,  } from "firebase/database";
+import { getDatabase, ref as r, set, onDisconnect,onValue, update, get, child  } from "firebase/database";
 import { useRoute } from 'vue-router'
 import { ref } from "vue";
 import { info } from "@/reactive";
 const route = useRoute()
 
 const qt = getDatabase()
-let gameInfo = ref(false)
 let name = info.name
 let reference = r(qt, `rooms/${route.params.code}`);
+let gameInfo = 'test'
+
 
 if(route.params.auth == 'host'){
     set(reference, {
       code: route.params.code,
-      players: 1,
+      minPlayers: 3,
+      aop: 1,
       joinable: true,
       players: {
         '1': {
@@ -29,22 +32,42 @@ if(route.params.auth == 'host'){
         }
       },
       state: 'start'
-    })
+    })      
 
 
     onValue(r(qt, `rooms/${route.params.code}`), (snapshot) => {
-  gameInfo.value = snapshot.val()
-  console.log(gameInfo.value)
+  gameInfo = snapshot.val()
+  console.log(gameInfo)
 });
+
     onDisconnect(reference).remove();
 }
 
 if(route.params.auth == 'join'){
-    console.log('ea')
-    await get(r(qt), `rooms/${route.params.code}/players`).then((snapshot) => { 
-        console.log(snapshot.val())
-    })
-}
+    onValue(r(qt, `rooms/${route.params.code}/`), (snapshot) => {
+  gameInfo = snapshot.val()
+});
+console.log(gameInfo)
+set(r(qt, `rooms/${route.params.code}/players/${gameInfo.aop + 1}`), {
+            pos: gameInfo.aop + 1,
+            name: name,
+            points: 0,
+  });
+
+
+  }
+
+
+
+
+//     await onValue(r(qt, `rooms/${route.params.code}`), (snapshot) => {
+//   gameInfo.value = snapshot.val()
+// });
+
+// let newaop = gameInfo.value.aop + 1
+// console.log(gameInfo.value)
+// update(reference, {aop: newaop})
+   
 
 
 </script>
