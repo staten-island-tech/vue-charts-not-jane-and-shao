92f33e3ec2eac35dc1c798e775b5674ccbd60460
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2024-03-25 13:13:50 -0400

    commit

---
+++ vite-project/src/components/selection.vue
@@ -0,0 +1,52 @@
+<script setup>
+const sprites = [
+    {
+        name: "businessMan",
+        img: "img link here",
+    },
+    {
+        name: "partyMan",
+        img: "img link here",
+    },
+    {
+        name: "surferGirl",
+        img: "img link here",
+    },
+    {
+        name: "pokemonTrainer",
+        img: "img link here",
+    },
+    {
+        name: "builderMan",
+        img: "img link here",
+    },
+    {
+        name: "IzzyMan",
+        img: "img link here",
+    },
+    {
+        name: "RylanMan (one pixel)",
+        img: "img link here",
+    },
+    {
+        name: "Noah(A)Man",
+        img: "img link here",
+    },
+    {
+        name: "gabe&noah(R)Man (poop emoji)",
+        img: "img link here",
+    },
+    {
+        name: "mike whalen (bald)",
+        img: "img link here",
+    },
+]
+</script>
+
+<template>
+    <div id="app">
+        <div v-for="sprite in sprites">
+            <div img ></div>
+        </div>
+    </div>
+</template>
