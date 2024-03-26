c43d80fa024598c6d7e93e7bc5372212e9533654
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2024-03-26 13:12:51 -0400

    commit

---
+++ vite-project/src/components/selection.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { ref } from 'vue'
+import reactive from '@/reactive'
+
+const sprites = [
+    {
+        name: "businessMan",
+        img: "img link here",
+    },
+    {
+        name: "partyMan",
+        img: "img link here",
+    },
+    {
+        name: "surferGirl",
+        img: "img link here",
+    },
+    {
+        name: "pokemonTrainer",
+        img: "img link here",
+    },
+    {
+        name: "builderMan",
+        img: "img link here",
+    },
+    {
+        name: "IzzyMan",
+        img: "img link here",
+    },
+    {
+        name: "RylanMan (one pixel)",
+        img: "img link here",
+    },
+    {
+        name: "Noah(A)Man",
+        img: "img link here",
+    },
+    {
+        name: "gabe&noah(R)Man (poop emoji)",
+        img: "img link here",
+    },
+    {
+        name: "mike whalen (bald)",
+        img: "img link here",
+    },
+]
+
+const characterSprite = ref('')
+const show = ref(0)
+
+function cycleF(){
+    show.value++
+    if(show.value > sprites.length){
+        show.value = 0
+    }
+    
+}
+
+function setCharacter(){
+    characterSprite.value = sprites.img
+}
+
+
+</script>
+
+<template>
+    <div id="app">
+        <button @click="cycleB()">last</button>
+        <img  :src="sprite[show.value].img" @click="setCharacter()">
+        <button @click="cycleF()">next</button>
+    </div>
+</template>
