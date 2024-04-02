1f789e5a06668602ba0cb7c3d72911a6fbef4720
rylan-geykhman <johnsmith102508@gmail.com>
2024-04-01 23:49:51 -0400

    commit

--- vite-project/src/components/endScreen.vue
+++ vite-project/src/components/endScreen.vue
@@ -11,10 +11,10 @@ import { ref, onMounted } from 'vue';
 let winner = ref('')
 let winnerPoints = ref(0)
 const props = defineProps({
-        selfNumber: Number,
-        gameInfo: Object,
-    })
-
+    selfNumber: Number,
+    gameInfo: Object,
+})
+const chartCanvas = ref(null);
     onMounted(async () => {
     console.log(props.gameInfo.players)
     props.gameInfo.players.forEach(player => {
@@ -27,6 +27,29 @@ const props = defineProps({
             winner.value = `${winner.value} and ${player.name}`
         }
     });
+    const ctx = chartCanvas.value.getContext('2d');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: [winner.value],
+      datasets: [{
+        label: 'Points',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+        data: [winnerPoints.value],
+      }],
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          },
+        }],
+      },
+    },
+  });
 })
 </script>
 
